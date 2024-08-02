ea77fc9ec50d30bc6271f94bf0656041fca33e05
SSIDDHARTHAS <srajawat@avaya.com>
2024-08-02 17:11:49 +0530

    Error Handling

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,9 @@
 fun main() {
-    val dog = Dog(3)
-    dog.introduce()
-    dog.bark()
-    dog.makeSound()
-
-    val cat = Cat()
-    cat.introduce()
-    cat.meow()
-    cat.makeSound()
-
-//    val animal = Animal("giraffe")
-    // not possible since Animal is abstract class
-
-    val bear = object: Animal("Bear"){
-        override fun makeSound() {
-            println("Growl")
-        }
+    val number = readlnOrNull() ?: "0"
+    val parsedNumber: Int = try {
+        number.toInt()
+    } catch (e: Exception){
+        0
     }
-    bear.introduce()
-    bear.makeSound()
+    println(parsedNumber)
 }
