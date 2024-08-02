29b99d7bbe95ea71705584f009bd3c01af2e96a6
SSIDDHARTHAS <srajawat@avaya.com>
2024-08-02 10:55:24 +0530

    Boolean Comparisions

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,10 @@
 fun main() {
-    val wholeNumber: Int = 3
-    val bigNumber: Long = 3L
-    val preciseDecimal: Double = 3.33
-    val pi: Float = 3.14f
-    val iLoveBhendi: Boolean = true
-    val iLoveTinde: Boolean = false
-    val iAmALetter: Char = 'a'
-    val iAmAWord: String = "abc"
+    val amIAnAdult = false
+    val amIAProgrammer = true
+    val amIAnAdultProgrammer = amIAnAdult and amIAProgrammer
+    val amIAnAdultOrAProgrammer = amIAnAdult or amIAProgrammer
+    println("Am I an adult programmer? : $amIAnAdultProgrammer")
+    println("Am I an adult or a programmer? : $amIAnAdultOrAProgrammer")
+    println(amIAProgrammer != amIAnAdult)
+    println(amIAProgrammer == amIAnAdult)
 }
