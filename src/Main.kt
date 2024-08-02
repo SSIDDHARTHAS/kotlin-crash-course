dd808916be57a269fdb5b28798768462538d5bb1
SSIDDHARTHAS <srajawat@avaya.com>
2024-08-02 14:39:49 +0530

    null safety

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,6 @@
-fun checker(x:Int) {
-    if(x == 2){
-        println("x is 2")
-    } else if(x == 3){
-        println("x is 3")
-    } else {
-        println("x is not 2 or 3")
-    }
-}
 fun main() {
-    checker(1+1)
-    checker(100)
-    val x = (Math.random() * 10).toInt()
-    val parity = if(x % 2 == 0) "even" else "odd"
-    println("$x is $parity number")
+    val number1 = readlnOrNull() ?: "0"
+    val number2 = readlnOrNull()
+    val result = number1.toInt() + number2!!.toInt()
+    println(result)
 }
