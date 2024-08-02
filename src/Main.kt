11a6018dc88b01942d60d68a12485ce0a2c6c817
SSIDDHARTHAS <srajawat@avaya.com>
2024-08-02 10:45:25 +0530

    Basic data types

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val wholeNumber: Int = 3
+    val bigNumber: Long = 3L
+    val preciseDecimal: Double = 3.33
+    val pi: Float = 3.14f
+    val iLoveBhendi: Boolean = true
+    val iLoveTinde: Boolean = false
+    val iAmALetter: Char = 'a'
+    val iAmAWord: String = "abc"
+}
