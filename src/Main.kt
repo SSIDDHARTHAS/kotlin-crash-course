f65299a7fc7612f885f32fabcec4e779cb6830ad
SSIDDHARTHAS <srajawat@avaya.com>
2024-08-02 15:21:54 +0530

    Function with default param

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,8 @@
 fun main() {
-    val marks = ((Math.random() * 100) + 1).toInt()
-    when(marks){
-        in 60..< 80 -> println("C")
-        85 -> println("S")
-        in 80..< 90 -> println("B")
-        in 90 .. 100 -> println("A")
-        else -> {
-            println("Congratulations, You played yourself")
-        }
-    }
+    greet("Sid")
+    greet()
+}
+
+fun greet(name: String = ""){
+    println("Hello $name")
 }
