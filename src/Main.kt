5c7384742e6824d785e1d92cf819b43574049c6b
SSIDDHARTHAS <srajawat@avaya.com>
2024-08-02 15:00:48 +0530

    Loops

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,26 @@
 fun main() {
     val shoppingList = mutableListOf("Lamborghini", "Penthouse", "Rolex")
     shoppingList.add("Ferrari")
-    println(shoppingList[3])
+
+    println("Printing using while loop")
+    var i = 0
+    while(i < shoppingList.size){
+        println(shoppingList[i])
+        i++
+    }
+
+    println("Printing using for in List")
+    for(item in shoppingList){
+        println(item)
+    }
+
+    println("Printing using while for in Range")
+    for(j in 1..< shoppingList.size){
+        println(shoppingList[j])
+    }
+
+    println("Printing using forEach")
+    shoppingList.forEach{
+        println(it)
+    }
 }
