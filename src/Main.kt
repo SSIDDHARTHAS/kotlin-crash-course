9c777273d31e4eb909b7430cb4fb8acaac65c894
SSIDDHARTHAS <srajawat@avaya.com>
2024-08-02 17:25:17 +0530

    Lambda functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,18 @@
 fun main() {
-    val number = readlnOrNull() ?: "0"
-    val parsedNumber: Int = try {
-        number.toInt()
-    } catch (e: Exception){
-        0
+    val list = listOf("Kotlin" , "is", "fun")
+    val count = list.customCount(check = { s ->
+        s.length == 3
+    }, "Hello")
+    println(count)
+}
+
+fun List<String>.customCount(check: (String) -> Boolean, msg: String): Int{
+    println(msg)
+    var count = 0
+    for(item in this){
+        if(check(item)){
+            count++
+        }
     }
-    println(parsedNumber)
+    return count
 }
